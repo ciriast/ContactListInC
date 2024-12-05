f7104be79bcaa6812c04b77946c7eab1248544d4
ciriast <ciriastorres@gmail.com>
2024-12-04 21:03:10 -0600

    Open a file with fopen

--- main.c
+++ main.c
@@ -2,8 +2,16 @@
 
 // Seems that from the standard library I can open files
 // I'll use a FILE structure to store the file returned by fopen
+// It was really fun to learn about paths in linux
 int main() {
-    printf("Hello from my contacts list app!");
+    FILE* config_file;
+
+    config_file = fopen("your path", "r");
+
+    if (config_file == NULL)
+        printf("The file couldn't be open\n");
+
+    printf("Hello from my contacts list app!\n");
     
     return 0;
 }
