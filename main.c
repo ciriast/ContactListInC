955e4573032a727c840b545901d5c410b9efda49
ciriast <ciriastorres@gmail.com>
2024-12-04 20:19:06 -0600

    Add documentation

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// Seems that from the standard library I can open files
+// I'll use a FILE structure to store the file returned by fopen
 int main() {
     printf("Hello from my contacts list app!");
     
